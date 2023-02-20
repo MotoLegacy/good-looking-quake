3e30ee9882ff963f428b7cc7e45683c0d4cd41f2
Steam Deck User <motolegacy.git@gmail.com>
2023-02-20 18:20:57 -0500

    Add OPT_FASTVECTORNORM for using faster rsqrt for normalizing

--- mathlib.c
+++ mathlib.c
@@ -366,6 +366,7 @@ vec_t Length(vec3_t v)
 
 float VectorNormalize (vec3_t v)
 {
+#ifndef OPT_FASTVECTORNORM
 	float	length, ilength;
 
 	length = v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
@@ -380,7 +381,18 @@ float VectorNormalize (vec3_t v)
 	}
 
 	return length;
-
+#else
+	//johnfitz -- courtesy of lordhavoc
+	float y, number;
+	number = DotProduct(v, v);
+	if (number != 0.0)
+	{
+		*((int *)&y) = 0x5f3759df - ((* (int *) &number) >> 1);
+		y = y * (1.5f - (number * 0.5f * y * y));
+		VectorScale(v, y, v);
+	}
+	return number;
+#endif // OPT_FASTVECTORNORM
 }
 
 void VectorInverse (vec3_t v)
